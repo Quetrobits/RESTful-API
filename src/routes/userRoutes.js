07f6309d28c771e8364829af091fc57f4ced7cf0
Quetro <lunamartinez55@outlook.com>
2024-12-11 16:06:43 +0100

    Commited the Routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
+
+router.get('/', getAllUsers);         
+router.get('/:id', getUserById);
+router.post('/', createUser);         
+router.put('/:id', updateUser);       
+router.delete('/:id', deleteUser);    
+
+module.exports = router;
